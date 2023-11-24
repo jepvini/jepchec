df2af57c4bd4125cc98d88343d145fecf20853ff
leo359531 <leos1359531@gmail.com>
2023-11-24 16:24:33 +0100

    added just first element check

--- checker.sh
+++ checker.sh
@@ -6,14 +6,25 @@ then
   exit
 fi
 
+if [ "$1" = "-a" ];
+then
+  if [ -z "$2" ];
+  then
+    echo "provide the folder"
+    exit
+  fi
+  for dir in "$2"/*/*
+  do
+    files=( "$dir"/*.flac )
+    flac -wt "$files"
+  done
+  exit
+fi
+
 for dir in "$1"/*/*/
 do
   echo "$dir"
   songs="$(find "$dir" -maxdepth 1 -type f)"
   songs="$(echo "$songs" | sed 's/\/\//\//')" # in could terminate with a /
   flac -wt "$dir"*.flac
-  if [ ! $? ];
-  then
-    echo "err"
-  fi
 done
